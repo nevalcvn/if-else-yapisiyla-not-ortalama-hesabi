4dbb05fb3abed111e1121485638ef4b4d06058fe
Neval <neval.civan97@gmail.com>
2026-06-30 08:18:36 +0300

    Add files via upload

---
+++ if_else_yapisiyla_not_ortalamasi_hesabi/if_else_yapisiyla_not_ortalamasi_hesabi.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+void main()
+{	//vize-->0.4 , final -->0.6 oranında etkileyecek.
+	int vize, final;
+	string ad;
+	cout << "Adiniz:";
+	cin >> ad;
+	cout << "Vize ve final notunuz:";
+	cin >> vize >> final;
+	double ortalama = (vize*0.4) + (final*0.6);
+	if (ortalama >= 90)
+	{
+		cout << "Tebrikler " << ad << ",not ortalaman: " << ortalama << ",harf notun:AA";
+	}
+	else if (ortalama >= 75)
+	{
+		cout << "Tebrikler " << ad << ",not ortalaman: " << ortalama << ",harf notun:BB";
+	}
+	else if (ortalama >= 60)
+	{
+		cout << "Tebrikler " << ad << ",not ortalaman: " << ortalama << ",harf notun:CC";
+	}
+	else if (ortalama >= 45)
+	{
+		cout << "Tebrikler " << ad << ",not ortalaman: " << ortalama << ",harf notun:DD";
+	}
+	else 
+	{
+		cout << "Dersten kaldin " << ad << ",not ortalaman: " << ortalama << ",harf notun:FF";
+	}
+}
+
